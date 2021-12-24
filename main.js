2212d98e0637a6cfc7c9fe4ae17d605eb37c36d1
Musaelyan <musaelyan.sergei@gmail.com>
2021-12-24 16:28:59 +0300

    Готова секция с описанием цветов

--- main.js
+++ main.js
@@ -160,4 +160,14 @@ function validateField(field){
 
 }
 
+// Для выдвижения контента в design "секция описания цветов"
+const design__title = $('.design__title');
+
+design__title.on('click', function(e){
+  design__title.not(this).closest('.design__item').removeClass('design__item-active');
+  $(this).closest('.design__item').toggleClass('design__item-active');
+  // console.log(this.closest('.member'))
+});
+
+
 });
